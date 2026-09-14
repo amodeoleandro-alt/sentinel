0078f958651b47ff6589d1a8abf76c904a8436eb
amodeoleandro-alt <amodeoleandro@gmail.com>
2026-09-14 13:31:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,11 @@ def create_app(env=None):
     return app
 
 
+# Creamos la instancia para que Gunicorn la encuentre al desplegar en Render
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
